f566fcd0be81558b8b1a62b1995a57851c9769be
Ben Johnson <bjohnson@binarylogic.com>
2017-02-24 17:33:05 -0600

    Make Heroku ready

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -5,7 +5,8 @@ defmodule ElixirPhoenixExampleApp.UserSocket do
   # channel "room:*", ElixirPhoenixExampleApp.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket,
+    timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
