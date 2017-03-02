e327f01bb49f18256a7b85568297446c41df772e
Ben Johnson <bjohnson@binarylogic.com>
2017-03-02 10:24:27 -0600

    Update timber

--- mix.exs
+++ mix.exs
@@ -38,7 +38,7 @@ defmodule ElixirPhoenixExampleApp.Mixfile do
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
      {:cowboy, "~> 1.0"},
-     {:timber, "~> 1.0"}]
+     {:timber, "~> 1.0", github: "timberio/timber-elixir", branch: "feature/simpler-exception-handling", override: true}]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
