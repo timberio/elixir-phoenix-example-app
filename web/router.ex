e9d1926844d58f18e14cf93a155ad47b8ea68f5c
Ben Johnson <bjohnson@binarylogic.com>
2017-03-02 10:26:13 -0600

    Update timber

--- web/router.ex
+++ web/router.ex
@@ -16,6 +16,7 @@ defmodule ElixirPhoenixExampleApp.Router do
   scope "/", ElixirPhoenixExampleApp do
     pipe_through :browser # Use the default browser stack
 
+    get "/boom", PageController, :boom
     get "/", PageController, :index
   end
 
