af74d1e46c5a23bae918929913a3937163829fd4
Ben Johnson <bjohnson@binarylogic.com>
2017-03-02 10:23:09 -0600

    Add exception

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,6 +1,10 @@
 defmodule ElixirPhoenixExampleApp.PageController do
   use ElixirPhoenixExampleApp.Web, :controller
 
+  def boom do
+    raise "boom"
+  end
+
   def index(conn, _params) do
     render conn, "index.html"
   end
